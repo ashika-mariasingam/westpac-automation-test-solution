df5ad2cd67f46975c1cb81129e8a29db96d33553
ashika-mariasingam <ashika.mariasingam@gmail.com>
2020-11-28 03:58:55 +1300

    Added Features, Stepdefs and Method definitions for Scenario 2

--- src/main/java/nz/co/westpac/controllers/NavigationController.java
+++ src/main/java/nz/co/westpac/controllers/NavigationController.java
@@ -1,12 +1,18 @@
 package nz.co.westpac.controllers;
 
 import nz.co.westpac.views.KiwiSaverRetirementCalculatorView;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class NavigationController {
 
@@ -27,6 +33,7 @@ public class NavigationController {
       }
    }
 
+
    public void OpenURL() {
            this.driver.manage().window().maximize();
            this.driver.get("http://www.westpac.co.nz/");
@@ -45,12 +52,116 @@ public class NavigationController {
     }
 
     public WebElement getKiwiSaverRetirementCalculatorIframe() {
+        //WebDriverWait wait = new WebDriverWait(this.driver, 20);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[1]")));
+        this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         return this.driver.findElement(By.xpath("//iframe[1]"));
     }
 
     public WebElement getKiwiSaverRetirementCalculatorInformationIconCurrentAge() {
-        return this.driver.findElement(By.xpath("//div[@help-id='CurrentAge']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/button[@class='icon-target-help-toggle']"));
+        WebDriverWait wait = new WebDriverWait(this.driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@help-id='CurrentAge']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/button")));
+        return this.driver.findElement(By.xpath("//div[@help-id='CurrentAge']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/button"));
     }
+
+    public WebElement getKiwiSaverCurrentAgeTextField() {
+        return this.driver.findElement(By.xpath("//div[@label='Current age']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[@type='text']"));
+    }
+
+    public WebElement getKiwiSaverSalary(){
+       return  this.driver.findElement(By.xpath("//div[@label='Salary or wages per year (before tax)']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[@type='text']"));
+    }
+    public WebElement getKiwiSaverEmploymentStatus() {
+        WebDriverWait wait = new WebDriverWait(this.driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+        return this.driver.findElement(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+    }
+
+    public WebElement getKiwiSaverEmploymentStatusEmployed(){
+        WebDriverWait wait = new WebDriverWait(this.driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[1]/div[1]")));
+        return  this.driver.findElement(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[1]/div[1]"));
+    }
+
+    public WebElement getKiwiSaverEmploymentStatusSelfEmployed(){
+        WebDriverWait wait = new WebDriverWait(this.driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[2]/div[1]")));
+        return  this.driver.findElement(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[2]/div[1]"));
+    }
+
+    public WebElement getKiwiSaverEmploymentStatusNotEmployed(){
+        WebDriverWait wait = new WebDriverWait(this.driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[3]/div[1]")));
+        return  this.driver.findElement(By.xpath("//div[@label='Employment status']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[3]/div[1]"));
+    }
+
+    public WebElement getKiwiSaverMemberContributionFour() {
+        return this.driver.findElement(By.xpath("//div[@label='KiwiSaver member contribution']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/label/span/input[@id='radio-option-04F']"));
+    }
+
+    public WebElement getKiwiSaverRiskProfileDefensive() {
+        return this.driver.findElement(By.xpath("//div[@label='Risk profile']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/label/span/input[@id='radio-option-013']"));
+    }
+
+    public WebElement getKiwiSaverRetirementCalculatorSubmit() {
+        return this.driver.findElement(By.xpath("//div[@class='widget-body']/div[1]/div[2]/button[@class='btn-results-reveal']"));
+    }
+
+    public WebElement getKiwiSaverBalance() {
+        return this.driver.findElement(By.xpath("//div[@help-id='KiwiSaverBalance']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[@type='text']"));
+    }
+
+    public WebElement getVoluntaryContribution(){
+       return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[@type='text']"));
+    }
+
+    public  WebElement getVoluntaryContributionFrequency(){
+       return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+    }
+
+
+    public WebElement getVoluntaryConributionWeekly(){
+        return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[2]"));
+    }
+
+    public WebElement getVoluntaryConributionFortnightly(){
+       return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[3]"));
+    }
+
+    public WebElement getVoluntaryConributionMonthly(){
+        return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[4]"));
+    }
+
+    public WebElement getVoluntaryContributionAnnually(){
+       return this.driver.findElement(By.xpath("//div[@help-id='VoluntaryContributions']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[5]"));
+    }
+
+    public WebElement getRiskProfileConservative(){
+        return this.driver.findElement(By.xpath("//div[@help-id='RiskProfile']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/label/span[1]/input[@id='radio-option-016']"));
+    }
+
+    public WebElement getRiskProfileBalanced(){
+        return this.driver.findElement(By.xpath("//div[@help-id='RiskProfile']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/label/span[1]/input[@id='radio-option-019']"));
+    }
+
+    public WebElement getRiskProfileGrowth(){
+        return this.driver.findElement(By.xpath("//div[@help-id='RiskProfile']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/label/span[1]/input[@id='radio-option-01C']"));
+    }
+
+    public WebElement getSavingsGoal(){
+        return this.driver.findElement(By.xpath("//div[@help-id='SavingsGoal']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[@type='text']"));
+    }
+
+    public WebElement getInformationIconElement(){
+       return  this.driver.findElement(By.xpath("//div[@class ='message-row ng-scope']/div[1]/p[1]"));
+    }
+
+
+
+
+
+
+
 
 
        public void NavigateToKiwiSaverRetirementCalculator () {
@@ -66,12 +177,121 @@ public class NavigationController {
         public void ClickOnInformationIcon(String fieldName){
             if(fieldName == "Current Age") {
                 driver.switchTo().frame(getKiwiSaverRetirementCalculatorIframe());
-                getKiwiSaverRetirementCalculatorInformationIconCurrentAge().click();
+                WebElement currentAgeButton = getKiwiSaverRetirementCalculatorInformationIconCurrentAge();
+                currentAgeButton.click();
             }
         }
 
+        public void EnterCurrentAge (String currentAge){
+            driver.switchTo().frame(getKiwiSaverRetirementCalculatorIframe());
+            getKiwiSaverCurrentAgeTextField().sendKeys(currentAge);
+        }
+
+         public void ChooseEmploymentStatus (String employmentStatus){
+             //driver.switchTo().frame(getKiwiSaverRetirementCalculatorIframe());
+             WebElement kiwiSaverEmploymentStatusElement = getKiwiSaverEmploymentStatus();
+             kiwiSaverEmploymentStatusElement.click();
+
+             switch (employmentStatus){
+                 case "Employed":
+                    WebElement kiwiSaverEmployedElement = getKiwiSaverEmploymentStatusEmployed();
+                     kiwiSaverEmployedElement.click();
+                 case "Self-employed":
+                     WebElement kiwiSaverSelfEmployedElement = getKiwiSaverEmploymentStatusSelfEmployed();
+                     kiwiSaverSelfEmployedElement.click();
+                 case "Not employed":
+                     WebElement kiwiSaverNotEmployedElement = getKiwiSaverEmploymentStatusNotEmployed();
+                     kiwiSaverNotEmployedElement.click();
+
+             }
+
+         }
+
+         public void EnterSalary(String salary){
+             if(StringUtils.isNotBlank(salary)) {
+                 getKiwiSaverSalary().sendKeys(salary);
+             }
+         }
+
+         public void ChooseKiwiSaverContribution (String kiwiSaverContribution){
+             WebElement kiwiSaverMemberContributionFourElement = getKiwiSaverMemberContributionFour();
+             kiwiSaverMemberContributionFourElement.click();
+         }
+
+         public void chooseRiskProfile (String riskProfile){
+            if(StringUtils.isNotBlank(riskProfile)) {
+                switch (riskProfile){
+                    case "Defensive":
+                         WebElement defensiveRiskProfileElement = getKiwiSaverRiskProfileDefensive();
+                         defensiveRiskProfileElement.click();
+                    case "Conservative":
+                        WebElement conservativeRiskProfileElement = getRiskProfileConservative();
+                        conservativeRiskProfileElement.click();
+                    case "Balanced":
+                        WebElement balancedRiskProfileElement = getRiskProfileBalanced();
+                        balancedRiskProfileElement.click();
+                    case "Growth":
+                        WebElement growthRiskProfileElement = getRiskProfileGrowth();
+                        growthRiskProfileElement.click();
+                }
+            }
+         }
+
+         public void ClickKiwiSaverSubmit(){
+             WebElement kiwiSaverRetirementSubmitElement = getKiwiSaverRetirementCalculatorSubmit();
+             kiwiSaverRetirementSubmitElement.click();
+         }
+
+          public void enterKiwiSaverBalance(String kiwiSaverBalance){
+              if(StringUtils.isNotBlank(kiwiSaverBalance)) {
+                  getKiwiSaverBalance().sendKeys(kiwiSaverBalance);
+              }
+          }
+
+          public void enterSavingGoals(String savingGoals){
+            if(StringUtils.isNotBlank(savingGoals)){
+                WebElement savingsTextElement = getSavingsGoal();
+                savingsTextElement.sendKeys(savingGoals);
+            }
+          }
+
+          public void entersVoluntaryContribution(String voluntaryContribution, String voluntaryContributionFrequency ){
+
+            if(StringUtils.isNotBlank(voluntaryContribution) && StringUtils.isNotBlank(voluntaryContributionFrequency)){
+                WebElement voluntaryContributionElement = getVoluntaryContribution();
+                WebElement voluntaryContributionFrequencyElement = getVoluntaryContributionFrequency();
+                WebElement voluntaryContributionWeeklyElement = getVoluntaryConributionWeekly();
+                WebElement voluntaryContributionFortnightlyElement = getVoluntaryConributionFortnightly();
+                WebElement voluntaryContributionMonthlyElement = getVoluntaryConributionMonthly();
+                WebElement voluntaryContributionAnnuallyElement = getVoluntaryContributionAnnually();
+                voluntaryContributionElement.sendKeys(voluntaryContribution);
+                voluntaryContributionFrequencyElement.click();
+                switch (voluntaryContributionFrequency){
+                    case "Weekly":
+                        voluntaryContributionWeeklyElement.click();
+                    case "Fortnightly":
+                        voluntaryContributionFortnightlyElement.click();
+                    case "Monthly":
+                        voluntaryContributionMonthlyElement.click();
+                    case "Annually":
+                        voluntaryContributionAnnuallyElement.click();
+
+                }
+
+            }
+
+          }
+
+          public void assertMessageDisplayed(){
+           String expectedMessage = "This calculator has an age limit of 64 years old as you need to be under the age of 65 to join KiwiSaver.";
+           String actualMessage = getInformationIconElement().getText();
+           assert(expectedMessage).equals(actualMessage);
+
+          }
+
+
     public WebDriver getWebDriver () {
-        return webDriverThreadLocal.get();
+       return webDriverThreadLocal.get();
     }
 
    }
--- src/test/java/nz/co/westpac/stepdefs/KiwiSaverRetirementCalculator.java
+++ src/test/java/nz/co/westpac/stepdefs/KiwiSaverRetirementCalculator.java
@@ -6,6 +6,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.And;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
 import nz.co.westpac.controllers.KiwiSaverRetirementCalculatorController;
 import nz.co.westpac.controllers.NavigationController;
 
@@ -19,12 +21,22 @@ public class KiwiSaverRetirementCalculator
      private final NavigationController navigationController = new  NavigationController();
     // private final KiwiSaverRetirementCalculatorController kiwiSaverRetirementCalculatorController = new KiwiSaverRetirementCalculatorController();
 
+     @Before
+     public void setup(){
+
+     }
+
+     @After
+     public void tearDown(){
+         navigationController.getWebDriver().close();
+     }
+
      @Given("^Open URL$")
      public void open_browser()  {
          navigationController.OpenURL();
      }
 
-     @And("^Navigate to the KiwiSaver Retirement Calculator page$")
+     @Then("^Navigate to the KiwiSaver Retirement Calculator page$")
      public void navigate_to_the_KiwiSaver_Retirement_Calculator_page() {
          navigationController.NavigateToKiwiSaverRetirementCalculator();
      }
@@ -36,7 +48,66 @@ public class KiwiSaverRetirementCalculator
 
      @Then("^Message is displayed$")
      public void message_is_displayed() {
+        navigationController.assertMessageDisplayed();
+     }
+
+     @Given("^User open browser and navigate to URL$")
+     public void user_open_browser_and_navigate_to_URL() {
+          navigationController.OpenURL();
+     }
+
+     @And("^User enter Current Age as (\\d+)$")
+     public void user_enter_Current_Age_as(String currentAge) {
+          navigationController.EnterCurrentAge(currentAge);
+     }
+
+     @And("^User choose Employment status \"([^\"]*)\"$")
+     public void user_choose_Employment_status(String employmentStatus)  {
+          navigationController.ChooseEmploymentStatus(employmentStatus);
+     }
+
+     @And("^User enter Salary as (.*)$")
+     public void user_enter_Salary_as(String salary)  {
+          navigationController.EnterSalary(salary);
+     }
+
+     @And("^User choose KiwiSaver contribution as (.*)$")
+     public void user_choose_KiwiSaver_contribution_as(String kiwiSaverContribution)  {
+          navigationController.ChooseKiwiSaverContribution(kiwiSaverContribution);
+     }
+
+     @And("^User choose Risk profile as \"([^\"]*)\"$")
+     public void user_choose_Risk_profile_as(String defensiveProfile)  {
+          navigationController.chooseRiskProfile(defensiveProfile);
+     }
+
+     @And("^User enters KiwiSaver balance as (.*)$")
+     public void user_enters_KiwiSaver_balance_as(String kiwiSaverBalance) {
+         navigationController.enterKiwiSaverBalance(kiwiSaverBalance);
+     }
+
+     @And("^User enters Savings goals as (.*)$")
+     public void user_enters_Savings_goals_as(String savingGoals) {
+           navigationController.enterSavingGoals(savingGoals);
+     }
+
+     @And("^User enters Voluntary Contribution as (.*) and (.*)$")
+     public void user_enters_Voluntary_Contribution_as(String voluntaryContribution, String voluntaryContributionFrequency )  {
+          navigationController.entersVoluntaryContribution(voluntaryContribution,voluntaryContributionFrequency);
+     }
+
+     @When("^User clicks the Complete form button$")
+     public void user_clicks_the_Complete_form_button(){
+          navigationController.ClickKiwiSaverSubmit();
+     }
+
+     @Then("^The correct results are displayed$")
+     public void the_correct_results_are_displayed()  {
 
      }
+
+
+
+
 
 }
